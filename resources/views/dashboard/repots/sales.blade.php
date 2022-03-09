e0ec22b6f123b627207afbe6ccf299b3c7000352
islamshu12 <islamshu12@gmail.com>
2022-03-09 16:28:24 +0200

    f

---
+++ resources/views/dashboard/repots/sales.blade.php
@@ -0,0 +1,84 @@
+@extends('layout.default')
+
+@section('content')
+<div class="card card-custom">
+
+    <div class="card-header">
+        <h3 class="card-title">
+            {{ __('Slaes Repots') }}
+        </h3>
+        <div class="card-toolbar">
+            <div class="example-tools justify-content-center">
+                <span class="example-toggle" data-toggle="tooltip" title="View code"></span>
+                <span class="example-copy" data-toggle="tooltip" title="Copy code"></span>
+            </div>
+        </div>
+    </div>
+    <form method="post" action="{{route('get_sales.store',['locale'=>app()->getLocale()])}}">
+        @csrf
+        @if ($errors->any())
+        <div class="alert alert-danger" role="alert">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+        @if (session()->has('message'))
+        <div class="alert {{session()->get('status')}} alert-dismissible fade show" role="alert">
+            <span> {{ session()->get('message') }}<span>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+        </div>
+        @endif
+        <div class="card-body">
+            <div class="row">
+
+                <div class="form-group col-md-6">
+
+                    <div class="form-group">
+                        
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <label class="input-group-text" for="inputGroupSelect01">
+                                    <i class="icon-pencil"></i>
+                                </label>
+                            </div>
+                          <input type="text" class="form-control" name="from">
+                        </div>
+                        @if($errors->has('country_id'))
+                        <p style="color: red">{{$errors->first('form')}}</p>
+                        @endif
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <label class="input-group-text" for="inputGroupSelect01">
+                                    <i class="icon-pencil"></i>
+                                </label>
+                            </div>
+                          <input type="text" class="form-control" name="to">
+                        </div>
+                        @if($errors->has('to'))
+                        <p style="color: red">{{$errors->first('to')}}</p>
+                        @endif
+                    </div>
+                </div>
+              
+            </div>
+
+        </div>
+        <div class="card-footer">
+            <button type="submit" class="btn btn-primary mr-2">Submit</button>
+            </div>
+    </form>
+</div>
+
+
+
+
+@endsection
+
+@section('scripts')
+
+@endsection

2f922be36ce9d344db906dbfd7426cdf0b87faea
islamshu12 <islamshu12@gmail.com>
2022-03-10 12:19:01 +0200

    d

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -88,9 +88,13 @@ class HomeController extends Controller
         }
         if($most_nat != '[]'){
             // dd($most_nat);
-            dd(count($most_nat));
-            $natonalitss = array_count_values($most_nat);
-            arsort($natonalitss);
+            if(count($most_nat) > 0 && $most_nat[0] != null){
+                $natonalitss = array_count_values($most_nat);
+                arsort($natonalitss);
+            }else{
+                $natonalitss = 0;
+            }
+           
         }else{
             $natonalitss = 0;
         }

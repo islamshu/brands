2c635a321617819de042bc28e11a3a1f228bc281
islamshu12 <islamshu12@gmail.com>
2022-03-10 10:57:34 +0200

    f

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -75,7 +75,7 @@ class HomeController extends Controller
             $count_branch = 0;
         }
         if($most_nat != null){
-            dd($most_nat);
+            dd($most_nat[0]);
             $natonalitss = array_count_values($most_nat);
             arsort($natonalitss);
         }else{
